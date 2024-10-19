7f6c0bf490bfd227749564ccd1a05f8ac785e5e6
Niksen111 <nikiksen2003@gmail.com>
2024-10-19 15:51:40 +0300

    Added docker configuration, created User entity

---
+++ src/main/java/com/example/niksen111/entity/User.kt
@@ -0,0 +1,23 @@
+package com.example.niksen111.entity
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "users")
+class User(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
+
+        @Column(nullable = false)
+        val name: String,
+
+        @Column(nullable = false, unique = true)
+        val email: String,
+
+        @Column(name= "created_at", nullable = false)
+        val createdAt: LocalDateTime
+) {
+    constructor() : this(0, "", "", LocalDateTime.now())
+}
