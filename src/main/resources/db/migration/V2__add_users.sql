f2c65d28df83775946b058196fd8ae78eeac5665
Niksen111 <nikiksen2003@gmail.com>
2024-10-19 17:56:21 +0300

    Added default users

---
+++ src/main/resources/db/migration/V2__add_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users values (1, 'Ivan Ivanov', 'ivanov@gmail.com', now());
+
+INSERT INTO users values (2, 'Petr Petrov', 'petrov@gmail.com', now());
+
+INSERT INTO users values (3, 'Denis Denisov', 'denisov@gmail.com', now());
