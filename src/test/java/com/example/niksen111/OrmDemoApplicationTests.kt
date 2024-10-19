e2778124c6dac75b523082c58816b3387e116a2c
Niksen111 <nikiksen2003@gmail.com>
2024-10-19 14:39:29 +0300

    Added test

---
+++ src/test/java/com/example/niksen111/OrmDemoApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.niksen111
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrmDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
